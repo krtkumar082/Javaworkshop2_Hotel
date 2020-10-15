f45a4c9a8c5c9f2bc2a249248be7ec45519a71d8
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-15 23:47:17 +0530

    [Kirti]Added:welcome message

---
+++ src/com/workshop/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.workshop;
+
+import java.util.*;
+
+public class HotelReservation {
+	public static void main(String[] args) {
+	System.out.println(" Welcome to hotel management system");
+    }
+}
